652b63722d484d1ea7f14d3f922af780acb09710
Munsterberg <jetrussell93@gmail.com>
2016-02-15 01:13:09 -0500

    add gulp, create gulpfile, fix method override

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-app.use(methodOverride());
+app.use(methodOverride('_method'));
 app.use(cookieParser());
 app.use(session({
   resave: true,
